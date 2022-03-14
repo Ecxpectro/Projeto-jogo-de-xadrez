5cd6b576357332c330c2eca278c3a7856cdb1c47
Ecxpectro <henriquescunha12345@gmail.com>
2022-03-14 11:01:48 -0300

    Comecando a implementar o tabuleiro

--- Jogo_de_xadrez/Program.cs
+++ Jogo_de_xadrez/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 namespace jogo_de_xadrez
 {
     class program
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
-            Console.WriteLine("Posição: " + p);
-            
+          Tabuleiro tab = new Tabuleiro(8, 8);
 
         }
     }
--- Jogo_de_xadrez/Tabuleiro/Posicao.cs
+++ Jogo_de_xadrez/Tabuleiro/Posicao.cs
@@ -1,4 +1,4 @@
-﻿namespace Tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
@@ -7,8 +7,8 @@
 
         public Posicao(int linha, int coluna)
         {
-            Linha = linha;
-            Coluna = coluna;
+            this.Linha = linha;
+            this.Coluna = coluna;
         }
         public override string ToString()
         {
