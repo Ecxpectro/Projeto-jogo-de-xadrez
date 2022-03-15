9dab4d7df468d0d077df363281fc9ec958588e59
Ecxpectro <henriquescunha12345@gmail.com>
2022-03-15 15:08:13 -0300

    Controlando as pecas capturadas

--- Jogo_de_xadrez/Program.cs
+++ Jogo_de_xadrez/Program.cs
@@ -15,10 +15,8 @@ namespace Jogo_de_xadrez
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
+                        
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
